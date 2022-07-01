f1e33dda4c5ad8690b0aff501afafcccb484643e
serdarsen <serdarsendev@gmail.com>
2022-07-01 19:00:23 +0300

    Linking Between Pages. Add airbnb eslint configuration.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 const NewsPage = () => (
-    <div>
-      News Page
-    </div>
-  );
-  
-  export default NewsPage;
-  
+  <>
+    <h1>The News Page</h1>
+    <ul>
+      <li>
+        <Link href="/news/nextjs-is-a-great-framework">
+          NextJS is a Great Framework
+        </Link>
+      </li>
+      <li>
+        <Link href="/news/something-else">
+          Something Else
+        </Link>
+      </li>
+    </ul>
+  </>
+);
+
+export default NewsPage;

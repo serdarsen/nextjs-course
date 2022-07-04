bca317934260334d0595616c8133b3f0bdd65f31
serdarsen <serdarsendev@gmail.com>
2022-07-04 06:43:54 +0300

    Introducing API Routes. Update Readme.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,4 +9,43 @@ const MeetupDetails = () => (
   />
 );
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data from a single meetup
+
+  const { params } = context;
+  const { meetupId } = params;
+
+  console.log("meetupId: ", meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: "https://images.wallpaperscraft.com/image/single/bridge_architecture_gothic_134848_1024x600.jpg",
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
